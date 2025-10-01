8cfa19060164cd99d9378b6a8d3b7cbfe6f90ad9
sannzay <sannzayreddy@gmail.com>
2025-09-30 20:28:23 -0400

    Use const for TextStyle and Text to improve performance

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,52 @@
 import 'package:flutter/material.dart';
+
 void main() {
-runApp(MyApp());
+  runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
-@override
-Widget build(BuildContext context) {
-return MaterialApp(
-home: FadingTextAnimation(),
-);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FadingTextAnimation(),
+    );
+  }
 }
-}
+
 class FadingTextAnimation extends StatefulWidget {
-@override
-_FadingTextAnimationState createState() => _FadingTextAnimationState();
+  @override
+  _FadingTextAnimationState createState() => _FadingTextAnimationState();
 }
+
 class _FadingTextAnimationState extends State<FadingTextAnimation> {
-bool _isVisible = true;
-void toggleVisibility() {
-setState(() {
-_isVisible = !_isVisible;
-});
-}
-@override
-Widget build(BuildContext context) {
-return Scaffold(
-appBar: AppBar(
-title: Text('Fading Text Animation'),
-),
-body: Center(
-child: AnimatedOpacity(
-opacity: _isVisible ? 1.0 : 0.0,
-duration: Duration(seconds: 1),
-child: Text(
-'Hello, Flutter!',
-style: TextStyle(fontSize: 24),
-),
-),
-),
-floatingActionButton: FloatingActionButton(
-onPressed: toggleVisibility,
-child: Icon(Icons.play_arrow),
-),
-);
-}
+  bool _isVisible = true;
+
+  void toggleVisibility() {
+    setState(() {
+      _isVisible = !_isVisible;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Fading Text Animation'),
+      ),
+      body: Center(
+        child: AnimatedOpacity(
+          opacity: _isVisible ? 1.0 : 0.0,
+          duration: const Duration(seconds: 1),
+          child: const Text(
+            'Hello, Flutter!',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: toggleVisibility,
+        child: const Icon(Icons.play_arrow),
+      ),
+    );
+  }
 }
